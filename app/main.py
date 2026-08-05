7973652faa058dac4b33948b58d368f5e3a74384
Saad-V <mohammedsaadsabeel@gmail.com>
2026-08-05 22:58:32 +0530

    added CORS

--- app/main.py
+++ app/main.py
@@ -4,7 +4,17 @@ from app.api.v1.interview_sessions import router as interview_router
 from app.api.v1.resumes import router as resume_router
 from app.api.v1.job_descriptions import router as job_description_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
